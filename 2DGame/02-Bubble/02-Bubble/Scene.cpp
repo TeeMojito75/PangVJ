880c2064f7855c421164e94eae3ee9997779935d
TeeMojito75 <iesm.joancarles.veny@gmail.com>
2024-03-27 09:49:29 +0100

    HUD complet

--- 2DGame/02-Bubble/02-Bubble/Scene.cpp
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -56,12 +56,9 @@ void Scene::init(const int& numLevel, int videsRest)
 
 	else if (over)
 	{
-		if (!gameO[0].init("fonts/DroidSerif.ttf"))
+		if (!gameO[0].init("fonts/DroidSerif.ttf") || !gameO[1].init("fonts/DroidSerif.ttf"))
 			cout << "Could not load font!!!" << endl;
 
-		if (!gameO[1].init("fonts/DroidSerif.ttf"))
-			cout << "Could not load font!!!" << endl;
-		
 		if (Game::instance().getKey(GLFW_KEY_ESCAPE))
 		{
 			Game::instance().init();
@@ -92,6 +89,12 @@ void Scene::init(const int& numLevel, int videsRest)
 		//if (!text[i].init("fonts/OpenSans-Regular.ttf"))
 		//if(!text.init("fonts/OpenSans-Bold.ttf"))
 		if(!text[i].init("fonts/DroidSerif.ttf"))
+			cout << "Could not load font!!!" << endl;
+	}
+
+	if (!over)
+	{
+		if(!gameO[0].init("fonts/DroidSerif.ttf") || !gameO[1].init("fonts/DroidSerif.ttf"))
 			cout << "Could not load font!!!" << endl;
 	}
 
@@ -176,6 +179,27 @@ void Scene::render()
 	if (vides >= 0) text[1].render("Vides: " + to_string(vides), glm::vec2(130, 840), 48, glm::vec4(1, 1, 1, 1));
 	if (temps > 0.f) text[2].render("Temps: " + to_string(int(temps)), glm::vec2(1050, 780), 48, glm::vec4(1, 1, 1, 1));
 	
+	if (!over)
+	{
+		if (auxLvl == 1)
+		{
+			gameO[0].render("Stage 1", glm::vec2(570, 780), 48, glm::vec4(1, 1, 1, 1));
+			gameO[1].render("Mt. Fuji", glm::vec2(570, 840), 48, glm::vec4(1, 1, 1, 1));
+		}
+
+		if (auxLvl == 2)
+		{
+			gameO[0].render("Stage 2", glm::vec2(570, 780), 48, glm::vec4(1, 1, 1, 1));
+			gameO[1].render("Lake", glm::vec2(600, 840), 48, glm::vec4(1, 1, 1, 1));
+		}
+
+		if (auxLvl == 3)
+		{
+			gameO[0].render("Stage 3", glm::vec2(570, 780), 48, glm::vec4(1, 1, 1, 1));
+			gameO[1].render("Muai Island", glm::vec2(520, 840), 48, glm::vec4(1, 1, 1, 1));
+		}
+	}
+
 	if (restart) 
 		gameO[0].render("PRESS C TO TRY AGAIN!!!", glm::vec2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), 64, glm::vec4(0, 0, 0, 1));
 	
